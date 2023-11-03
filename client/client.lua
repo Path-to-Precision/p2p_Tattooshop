7b4244073095af12be64116a4a347570ab698c06
Powo <94808786+Powoo@users.noreply.github.com>
2023-11-03 11:30:51 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -33,7 +33,6 @@ end
 subMenu.Closed = function()
     RenderScriptCams(false, true, 1000, true, true)
     DestroyAllCams(true)
-	DisplayRadar(true)
 end
 
 subMenu:AddInstructionButton({[1] = GetControlInstructionalButton(0, Config.key.tournerdroite, 0), [2] = Config.Translate('cam_right')}) 
@@ -196,7 +195,6 @@ function Tattoos()
                 RageUI.IsVisible(subMenu,function()
                     subMenu:UpdateInstructionalButtons(true)
                     LoadCam()
-			DisplayRadar(false)
                     PointCamAtCoord(cam, GetOffsetFromEntityInWorldCoords(PlayerPedId(), array.cam))
                     for _,tattoo in pairs(allTattoos) do
                         if tattoo.Zone == array.zone then
